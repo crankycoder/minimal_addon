a2ba7f24bf64ead008e43bd03d366f23e9550a66
Victor Ng <victor@crankycoder.com>
2015-09-30 14:19:17 -0400

    reworked addon code to use a single content script

--- data/geo-script.js
+++ data/geo-script.js
@@ -66,3 +66,13 @@ if (self.options.showOptions) {
         }
     }
 }
+
+function startOfflineScan() {
+    // This method is just a dumb proxy to initiate a wifi scan
+    // within the chrome process.  You can find the actual
+    // offline scan code in index.js and looking for a
+    // worker.port.on("run_emscr_wget") block.
+    self.port.emit("run_emscr_wget", "ignored_blob");
+}
+
+exportFunction(startOfflineScan, unsafeWindow, {defineAs: "startOfflineScan"});
--- index.js
+++ index.js
@@ -7,18 +7,22 @@ var {Cc, Ci, Cu, Cr, Cm, components} = require("chrome");
 var page = pageMod.PageMod({
     include: "*",
     contentScriptWhen: "start",
-    contentScriptFile: [data.url("geo-script.js"), data.url("customWifi.js")],
+    contentScriptFile: [data.url("geo-script.js")],
     contentScriptOptions: {
         showOptions: true
     },
     onAttach: function(worker) {
                   worker.port.on("run_emscr_wget", function(addonMessage) {
                       console.log("Addon received message: ["+addonMessage+"]");
-                      var addon_geo = require("./emscr/addon_geo");
-                      console.log("Addon Geo: " + addon_geo);
+                      try {
+                          var addon_demo_mod = require("./emscr/addon_demo").factory();
+                          console.log("Addon Demo Mod: " + addon_demo_mod);
 
-                      var addon_geo_mod = addon_geo.factory();
-                      console.log("Addon Geo Mod: " + addon_geo_mod);
+                          addon_demo_mod._start_demo();
+                          console.log("Completed run.  Everything is ok!");
+                      } catch (e) {
+                          console.log(e);
+                      }
                   });
               }
 });
